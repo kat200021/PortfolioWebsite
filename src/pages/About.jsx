6d54ebb8a7732eb60c8de78188046f6234f1a8ec
kat200021 <shreyareddy.katanguru@gmail.com>
2025-04-29 21:06:38 -0400

    updated profile picture path

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -88,7 +88,7 @@ const About = () => {
             >
               <div className="aspect-w-3 aspect-h-4 rounded-xl overflow-hidden shadow-lg">
                 <img
-                  src="/assets/ProfilePicture.jpg"
+                  src="./assets/ProfilePicture.jpg"
                   alt=' '
                   className="w-full h-full object-center object-cover"
                 />
